8c71ed00b8694599c221df54a4008e177cf0e072
EulerLagrange.eth <hgpatel715@gmail.com>
2024-06-26 18:29:13 -0400

    small script change

--- generate-notary-keys.sh
+++
@@ -1,4 +0,0 @@
-openssl ecparam -name secp256r1 -genkey -noout -out fixture/notary/private-key
-openssl pkcs8 -topk8 -inform PEM -outform PEM -in fixture/notary/private-key -out fixture/notary/notary.key -nocrypt
-openssl ec -in fixture/notary/notary.key -pubout -out fixture/notary/notary.pub
-rm fixture/notary/private-key
---
+++ generate_notary_keys.sh
@@ -0,0 +1,26 @@
+
+
+# Root certificate
+openssl req -newkey rsa:2048 -nodes -keyout fixture/tls/rootCA.key -x509 -days 365 -out fixture/tls/rootCA.crt -subj "/C=US/ST=L1/L=EigenLayer/O=Opacity/CN=OpacityNetwork"
+openssl req -x509 -sha256 -new -nodes -key fixture/tls/rootCA.key -days 3650 -out fixture/tls/rootCA.pem -subj "/C=US/ST=L1/L=EigenLayer/O=Opacity/CN=OpacityNetwork"
+
+
+# Notary certificate
+openssl req -newkey rsa:2048 -nodes -days 365000 \
+   -keyout fixture/tls/notary.key \
+   -out fixture/tls/notary.csr \
+   -subj "/C=US/ST=L1/L=EigenLayer/O=Opacity/CN=OpacityNetwork"
+
+
+openssl x509 -req -days 365000 -set_serial 01 \
+   -in fixture/tls/notary.csr \
+   -out fixture/tls/notary.crt \
+   -CA fixture/tls/rootCA.pem \
+   -CAkey fixture/tls/rootCA.key \
+   -extfile fixture/tls/notary.ext
+
+
+openssl ecparam -name secp256r1 -genkey -noout -out fixture/notary/private-key
+openssl pkcs8 -topk8 -inform PEM -outform PEM -in fixture/notary/private-key -out fixture/notary/notary.key -nocrypt
+openssl ec -in fixture/notary/notary.key -pubout -out fixture/notary/notary.pub
+rm fixture/notary/private-key
