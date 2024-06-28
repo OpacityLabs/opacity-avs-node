a73eeb0892834e14cc9870241d8539471a266a74
EulerLagrange.eth <hgpatel715@gmail.com>
2024-06-28 17:25:55 -0400

    Trimming

--- create-cluster.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# eksctl create cluster --name zktls-cluster-dev --fargate
-# aws eks update-kubeconfig --name zktls-cluster-dev
-# aws ecr create-repository --repository-name opacity-zktls-registry
-
-aws ecr describe-repositories \
-      --repository-names opacity-zktls-registry
