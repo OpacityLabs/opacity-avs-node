ffd83681a4fe917193ccca44ac0d521dfa958b9f
EulerLagrange.eth <hgpatel715@gmail.com>
2024-07-04 20:25:29 -0400

    Showing operator info

---
+++ src/wallet.rs
@@ -0,0 +1,19 @@
+use ethers::{
+    core::k256::{ecdsa::SigningKey, Secp256k1},
+    signers::{LocalWallet, Signer, Wallet},
+};
+
+pub fn load_oeprator_wallet(
+    ecdsa_key_path: &String,
+    password: &String,
+) -> Result<Wallet<SigningKey>, Box<dyn std::error::Error>> {
+    // Generate a random wallet
+    let wallet_result = LocalWallet::decrypt_keystore(ecdsa_key_path, password);
+
+    let wallet = match wallet_result {
+        Ok(signer) => signer,
+        Err(error) => panic!("Problem loading ECDSA key: {error:?}"),
+    };
+
+    Ok(wallet)
+}
