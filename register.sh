94e8670884738a299340880f1071246bb15f7b62
0xwildhare <sschell1979@gmail.com>
2025-02-05 13:22:04 -0800

    Revert "refactor: improve register.sh with binary existence check and explicit path"
    This reverts commit 00a97359afcf3864abc3a778888fd7951ceb29de.

--- register.sh
+++ register.sh
@@ -1,9 +1,3 @@
-#!/bin/sh
-set -e  # Exit on any error
-
-if [ ! -f "/opacity-avs-node/target/release/register" ]; then
-    echo "Error: register binary not found"
-    exit 1
-fi
-
-/opacity-avs-node/target/release/register /opacity-avs-node/config/opacity.config.yaml 
+#!/bin/bash
+set -e
+make register-opacity-node
